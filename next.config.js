fe7c049a12cab085643501198c38aa71c44062d2
Nilesh Bhimani <46840260+nileshbhimani21@users.noreply.github.com>
2023-10-28 09:57:37 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,8 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,  
+trailingSlash: true,
+   //productionBrowserSourceMaps: true,
   experimental: {
     appDir: true,
+  },
+images: {
+    domains: [process.env.NEXT_PUBLIC_ALLOWED_IMAGE_DOMAINS],
+  },
+webpack: (config) => {
+    config.experiments = config.experiments || {}
+    config.experiments.topLevelAwait = true
+    return config
   },
 }
 
